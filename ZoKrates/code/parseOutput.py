9ad51f3cf84a51f965ab137acbbeae3fa722a85a
Nicolas Donnaint <ashcatran@gmail.com>
2019-03-23 11:46:47 +0100

    Mise a jour de parseoutput

--- ZoKrates/code/parseOutput.py
+++ ZoKrates/code/parseOutput.py
@@ -18,17 +18,13 @@ if __name__ == "__main__":
 		print("bad input, will not be displayed")
 	with open("output.txt", "r") as f:
 		data = f.read()
+		data = data.split('Proof:\n')[1].split('\ngenerate-proof successful: true\n')[0]
 		data = data.split('\n')
 		res = ""
 		result_list = []
 		for line in data:
-			#print("###")
-			#print(line)
-			#print(len(line))
-			#print("###")
 			if len(line) > 0: 
 				ls = line.split('(')[-1].split(')')[0]
-				#print(ls)
 				if ls[0] == '0':
 					ls = ls.split(', ')
 					res = '["' + ls[0] + '", "' + ls[1] + '"]'
@@ -39,9 +35,7 @@ if __name__ == "__main__":
 					res = res.replace(',', '')
 					res = res.split(' ')
 					res = '[["' + res[0] + '", "' + res[1] + '"], ["' + res[2] + '", "' + res[3] + '"]]'
-					#print(res)
 				result_list.append(res)
-			#print(line)
 		if is_input:
 			result_list.append(user_input)
 		print("### LIST ###")
